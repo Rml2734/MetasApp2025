dd04d91619b0f985ca9c4f9476b4fd0490b15abc
Robml2734 <Robml2734@gmail.com>
2025-01-24 18:35:23 -0600

    Cambios tailwind

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Encabezamiento from './componentes/compartidos/Encabezamiento';
+import Pie from './componentes/compartidos/Pie';
+import Principal from './componentes/compartidos/Principal';
+
+
+function App() {
+  return (
+      <div className="App">
+        <Encabezamiento></Encabezamiento>
+        <Principal children={undefined}></Principal>
+        <Pie></Pie>
+      </div>
+  );
+}
+
+export default App
---
+++ src/componentes/compartidos/Pie.tsx
@@ -0,0 +1,12 @@
+function Pie() {
+  return (
+    <footer className="text-red-500">
+      <small>
+        Desarrollado y diseñado por Roberto Martinez Ledezma. ©2025. Todos los
+        derechos reservados.
+      </small>
+    </footer>
+  );
+}
+
+export default Pie;
