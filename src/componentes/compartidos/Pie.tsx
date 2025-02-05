8f9ae0b1430f6a62bf87bcbef28da155730162ee
Robml2734 <Robml2734@gmail.com>
2025-02-04 20:52:30 -0600

    Workin 2

--- src/componentes/compartidos/Pie.tsx
+++ src/componentes/compartidos/Pie.tsx
@@ -1,7 +1,9 @@
+import styles from "./Pie.module.css";
+
 function Pie() {
   return (
-    <footer className="bg-red-500 text-white p-4 m-4 rounded-lg shadow-md">
-      <small>
+    <footer className={styles.pie}>
+      <small >
         Desarrollado y diseñado por Roberto Martinez Ledezma. ©2025. Todos los
         derechos reservados.
       </small>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins:
-[react(),
-svgr({
-// svgr options: https://react-svgr.com/docs/options/
-svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
-include: "**/*.svg",
-}),
-],
+  plugins: [
+    react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: "named",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+        icon: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
 });
